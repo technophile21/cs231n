5970d5026681959f7a4b980ac50f9c7605ebd0e5
Manik <technophile21@gmail.com>
2017-12-23 16:11:27 +0530

    knn classifier implementation

--- assignment1/cs231n/classifiers/k_nearest_neighbor.py
+++ assignment1/cs231n/classifiers/k_nearest_neighbor.py
@@ -1,5 +1,5 @@
 import numpy as np
-from past.builtins import xrange
+#from past.builtins import xrange
 
 
 class KNearestNeighbor(object):
@@ -73,7 +73,10 @@ class KNearestNeighbor(object):
         # training point, and store the result in dists[i, j]. You should   #
         # not use a loop over dimension.                                    #
         #####################################################################
-        pass
+        diff = X[i] - self.X_train[j]
+        sqr = np.square(diff)
+        l2 = np.sqrt(sqr.sum())
+        dists[i][j] = l2
         #####################################################################
         #                       END OF YOUR CODE                            #
         #####################################################################
@@ -95,7 +98,10 @@ class KNearestNeighbor(object):
       # Compute the l2 distance between the ith test point and all training #
       # points, and store the result in dists[i, :].                        #
       #######################################################################
-      pass
+      diff = X[i] - self.X_train
+      sqr = np.square(diff)
+      l2 = np.sqrt(sqr.sum(axis=1))
+      dists[i] = l2.T
       #######################################################################
       #                         END OF YOUR CODE                            #
       #######################################################################
@@ -123,7 +129,16 @@ class KNearestNeighbor(object):
     # HINT: Try to formulate the l2 distance using matrix multiplication    #
     #       and two broadcast sums.                                         #
     #########################################################################
-    pass
+    X_new = np.sum(np.square(X), axis=1)
+    X_train_new = np.sum(np.square(self.X_train), axis=1)
+
+    product = np.dot(X, self.X_train.T)
+    #print(X_new.shape)
+    #print(X_train_new.shape)
+    #print(product.shape)
+    sum_new = X_new.reshape(-1, 1) + X_train_new
+    #print(sum_new.shape)
+    dists = np.sqrt(sum_new - 2 * product)
     #########################################################################
     #                         END OF YOUR CODE                              #
     #########################################################################
@@ -144,6 +159,8 @@ class KNearestNeighbor(object):
     """
     num_test = dists.shape[0]
     y_pred = np.zeros(num_test)
+    sorted_indx = np.argsort(dists, 1)
+    k_indx = list(range(k))
     for i in xrange(num_test):
       # A list of length k storing the labels of the k nearest neighbors to
       # the ith test point.
@@ -155,7 +172,9 @@ class KNearestNeighbor(object):
       # neighbors. Store these labels in closest_y.                           #
       # Hint: Look up the function numpy.argsort.                             #
       #########################################################################
-      pass
+      k_neighbors = sorted_indx[i][k_indx]
+      k_neigh_class = self.y_train[k_neighbors]
+
       #########################################################################
       # TODO:                                                                 #
       # Now that you have found the labels of the k nearest neighbors, you    #
@@ -163,7 +182,27 @@ class KNearestNeighbor(object):
       # Store this label in y_pred[i]. Break ties by choosing the smaller     #
       # label.                                                                #
       #########################################################################
-      pass
+      k_neigh_class.sort()
+      last_cls = -1
+      max_cnt = 1
+      cnt = 0
+      max_class = k_neigh_class[0]
+      for cls in k_neigh_class:
+        if(cls == last_cls):
+          cnt = cnt + 1
+        else:
+          if(max_cnt < cnt):
+            max_cnt = cnt
+            max_class = last_cls
+          cnt = 1
+
+        last_cls = cls
+
+      if(max_cnt < cnt):
+        max_cnt = cnt
+        max_class = last_cls
+
+      y_pred[i] = max_class
       #########################################################################
       #                           END OF YOUR CODE                            # 
       #########################################################################
